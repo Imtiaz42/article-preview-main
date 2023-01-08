df2014d864b9e2ad1bc55b926e399937e10f6ce2
Imtiaz42 <118330561+Imtiaz42@users.noreply.github.com>
2023-01-08 16:33:26 +0500

    Add files via upload

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,19 @@
+document.getElementById("btn").addEventListener('click', function (){
+    document.querySelector('.share').style.display = 'flex';
+    document.getElementById("btn-close").style.display = 'inline-block';
+    document.getElementById("btn").style.display = 'none';
+
+});
+
+function init(){
+    document.querySelector('.share').style.display = 'none';
+    document.getElementById("btn").style.display = 'inline-block';
+    document.getElementById("btn-close").style.display = 'none';
+};
+
+document.getElementById("btn-close").addEventListener('click',init);
+document.getElementById("btn-closeOne").addEventListener('click',init());
+
+
+
+
